b991fdadb92e4e78167ca1c1d1a85059e3947548
Birgir Hrafn <biggihs@gmail.com2>
2013-11-05 09:37:21 +0000

    Rails Admin Custom Root Action

--- lib/rails_admin_guest_list.rb
+++ lib/rails_admin_guest_list.rb
@@ -1,4 +1,5 @@
 require "rails_admin_guest_list/engine"
+require 'rails_admin/config/sections/base'
 
 module RailsAdminGuestList
 end
@@ -10,10 +11,33 @@ module RailsAdmin
     module Actions
       class GuestList < Base
         RailsAdmin::Config::Actions.register(self)
-        
-        register_instance_option :object_level do
+
+        register_instance_option :visible? do
+          authorized? #&& !bindings[:object].approved
+        end
+
+        register_instance_option :root? do
           true
         end
+
+        register_instance_option :link_icon do
+#          'icon-book'
+          'icon-list-alt'
+        end
+
+        register_instance_option :i18n_key do
+          'guest_list'
+        end
+
+        register_instance_option :controller do 
+          Proc.new do
+            respond_to do |format|
+              format.html {render @action.template_name}
+              format.js {render @action.template_name, :layout => false}
+            end
+          end
+        end
+
       end
     end
   end
