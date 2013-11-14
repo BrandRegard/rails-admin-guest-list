97b58b025f55b8d02d030df8b16831b20a7e3a2a
Birgir Hrafn <biggihs@gmail.com2>
2013-11-14 14:42:22 +0000

    Add increment/decrement tickets for created bookings

--- lib/rails_admin_guest_list.rb
+++ lib/rails_admin_guest_list.rb
@@ -7,11 +7,52 @@ end
 module RailsAdminCreateBooking
 end
 
+module RailsAdminUpdateBooking
+end
+
 require 'rails_admin/config/actions'
 
 module RailsAdmin
   module Config
     module Actions
+
+      class UpdateBooking < Base
+        RailsAdmin::Config::Actions.register(self)
+
+        # Should the action be visible
+        register_instance_option :visible? do
+          false
+        end
+
+        register_instance_option :root? do
+          true
+        end
+
+        register_instance_option :controller do 
+          Proc.new do
+            respond_to do |format|
+              format.html do 
+                flash[:notice] = "Incorrect Booking Method"
+                redirect_to request.referer
+              end
+              format.json {
+                begin
+                  id = params[:booking][:id]
+                  ticket_count = params[:booking][:ticket_count].to_i
+                  booking = Booking.find(id)
+                  new_ticket_count = booking.alter_ticket_count(ticket_count)
+                  total_sold = booking.event.bookings.map(&:tickets).flatten.count 
+                  sold_out = (total_sold == booking.event.place) 
+                  render :json => {:success => true  , :booking_id=> id, :total_sold_tickets=>total_sold ,:sold_out=>sold_out, :ticket_count => new_ticket_count}
+                rescue Exception => e
+                  render :json => {:success => false , :booking_id=> id, :errors => e.message}
+                end
+              }
+            end
+          end
+        end
+      end
+
       class CreateBooking < Base
         RailsAdmin::Config::Actions.register(self)
 
